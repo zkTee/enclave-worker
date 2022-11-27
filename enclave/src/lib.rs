d3ad51c5538393c3d6534071f77ec1d5026dc747
zTgx <747674262@qq.com>
2022-11-27 21:05:12 +0800

    rename enclave

--- enclave/src/lib.rs
+++ enclave/src/lib.rs
@@ -15,7 +15,7 @@
 // specific language governing permissions and limitations
 // under the License..
 
-#![crate_name = "helloworldsampleenclave"]
+#![crate_name = "enclave"]
 #![crate_type = "staticlib"]
 
 #![cfg_attr(not(target_env = "sgx"), no_std)]
